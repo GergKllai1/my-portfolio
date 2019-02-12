036a23ac18a5375080f8fdce72c04287704670db
Gregory Kallai <gerg.kallai@gmail.com>
2019-02-12 11:20:03 +0100

    Changes background image, centers text

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route } from "react-router-dom"
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from './Home';
 import Projects from './Projects';
 import About from './About';
--- src/Home.jsx
+++ src/Home.jsx
@@ -4,8 +4,13 @@ import './Home.css'
 const Home = () => {
     return (
         <div className='main'>
-            <div className='color-white'>
-                Home
+            <div className='text-white'>
+                <div className='d-flex justify-content-center align-items-center'>
+                <div className='d-flex flex-column'>
+                    <h1 className='border-bottom'>Gregory Kallai</h1>
+                    <h3>Full Stack Developer</h3>
+                </div>
+                </div>
             </div>
         </div>
     )
