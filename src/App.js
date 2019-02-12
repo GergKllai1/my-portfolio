4d269dcc2510d83477053e0287918996c6a7a08d
Gregory Kallai <gerg.kallai@gmail.com>
2019-02-12 09:10:59 +0100

    Sets up the 3 components as per the planned LoFi

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom"
 import Home from './Home';
+import Projects from './Projects';
+import About from './About';
 
 class App extends Component {
   render() {
@@ -8,6 +10,8 @@ class App extends Component {
       <BrowserRouter>
         <Switch>
           <Route exact path='/' component={Home} />
+          <Route exact path='/projects' component={Projects} />
+          <Route exact path='/about' component={About} />
         </Switch>
       </BrowserRouter>
     );
