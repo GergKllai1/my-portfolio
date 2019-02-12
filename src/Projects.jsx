a8c5e768a8d992c71f1996d6c634d280cb9d7b00
Gregory Kallai <gerg.kallai@gmail.com>
2019-02-12 13:22:35 +0100

    Adds projects json file

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
 
 export default class Projects extends Component {
+    constructor() {
+        super();
+        this.state = {
+            projects: ''
+        }
+    }
+
     render() {
         return (
             <div>
