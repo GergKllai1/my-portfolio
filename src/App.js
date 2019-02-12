0ccfff9908177ffd9a8051f2318ce49ba1e2bf55
Gregory Kallai <gerg.kallai@gmail.com>
2019-02-12 13:16:17 +0100

    Removes transtition

--- src/App.js
+++ src/App.js
@@ -3,23 +3,17 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from './Home';
 import Projects from './Projects';
 import About from './About';
-import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <CSSTransition
-          timeout={300}
-          classNames='fade'
-        >
           <Switch>
             <Route exact path='/' component={Home} />
             <Route exact path='/projects' component={Projects} />
             <Route exact path='/about' component={About} />
           </Switch>
-        </CSSTransition>
       </BrowserRouter>
     );
   }
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
-import {Transition} from 'react-transition-group'
 import './Home.css'
 
 const Home = () => {
