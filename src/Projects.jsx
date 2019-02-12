051a9d4c0609972d7693b63ee33547e321fdd2ea
Gregory Kallai <gerg.kallai@gmail.com>
2019-02-12 13:38:29 +0100

    Fetches the data from json file

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 export default class Projects extends Component {
     constructor() {
         super();
         this.state = {
-            projects: ''
+            projects: []
         }
     }
 
+    componentDidMount() {
+        axios.get('projects.json')
+            .then(response => {
+                console.log(response)
+                this.setState({
+                    projects: response.data
+                });
+            });
+    }
     render() {
         return (
             <div>
