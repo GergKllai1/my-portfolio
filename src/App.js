881c378e34ee0b069563773c1471065da04c512c
Gregory Kallai <gerg.kallai@gmail.com>
2019-02-13 08:14:48 +0100

    Refactors components, adds browserrouter to index

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Home from './Home';
 import Projects from './Projects';
 import About from './About';
@@ -8,13 +8,11 @@ import About from './About';
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/projects' component={Projects} />
-            <Route exact path='/about' component={About} />
-          </Switch>
-      </BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/projects' component={Projects} />
+          <Route exact path='/about' component={About} />
+        </Switch>
     );
   }
 }
