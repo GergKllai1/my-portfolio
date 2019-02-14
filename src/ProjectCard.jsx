66627d6e451ecbdbee1d045f77e393ebfb74a3ff
Gregory Kallai <gerg.kallai@gmail.com>
2019-02-14 09:09:11 +0100

    Adds modal popup starts rewriting descriptions

--- src/ProjectCard.jsx
+++ src/ProjectCard.jsx
@@ -1,19 +1,26 @@
 import React from 'react'
-
+import Popup from 'reactjs-popup'
 
 function ProjectCard(props) {
     let project = props.project
     return (
         <div className='card m-4 bg-light shadow-lg' style={{ width: '20rem', height: '23rem' }}>
-            <img style={{ width: '20rem', height: '10rem' }} src={project.image} alt="" />
+            <Popup 
+                trigger={<img className='img' style={{ width: '20rem', height: '10rem' }} src={project.image} alt="" />}
+                modal
+                closeOnDocumentClick
+                >
+                <h4>{project.description1}</h4>
+                <p>{project.description2}</p>
+            </Popup>
             <div className='card-body'>
                 <h3 className='card-title pb-2'>{project.name}</h3>
                 <div className='d-flex pb-4'>
                     <a href={project.github} target='_blank'>
                         <img className='pr-4' style={{ height: '30px' }} src="https://image.flaticon.com/icons/png/512/37/37318.png" alt="" />
                     </a>
-                    <a href={project.website} target='_blank' style={ project.website ? {} : {display: 'none'}}>
-                        <img className='pr-4'style={{ height: '30px' }} src="https://openclipart.org/image/300px/svg_to_png/216096/WWW-Icon.png" alt="" />
+                    <a href={project.website} target='_blank' style={project.website ? {} : { display: 'none' }}>
+                        <img className='pr-4' style={{ height: '30px' }} src="https://openclipart.org/image/300px/svg_to_png/216096/WWW-Icon.png" alt="" />
                     </a>
                 </div>
                 <h5>Techonologies used:</h5>
